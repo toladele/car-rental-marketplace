c74ee3ad4e3b3609852b35472db809efbf83d292
toladele <tommy.oladele@mail.utoronto.ca>
2021-02-23 14:15:04 -0500

    level 2: filter duration and distance

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import styled, { ThemeConsumer } from "styled-components";
 import Deezer from "/images/deezer-logo.png";
 import Paper from "@material-ui/core/Paper";
 import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/core/Slider';
 
 const HeaderContainer = styled.div`
   width: 100%;
@@ -14,21 +15,25 @@ const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const Logo = styled.img`
   width: 20vw;
-`;
+`
 
 const CarInfo = styled.div`
 font-size: 1.5em;
+`
+
+const FilterInfo = styled.div`
+font-size: 1em;
 `
 
 const styles = {
   header: {
     boxShadow: "1px 2px 4px 2px grey",
   },
-  paper: {
+  carPaper: {
     backgroundColor: "transparent" as const, 
     verticalAlign: "middle" as const,
     textAlign: "center" as const,
@@ -39,6 +44,17 @@ const styles = {
     borderImageSlice: 1 as const,
     boxShadow: "1px 3px 1px grey" as const,
   },  
+  filtersPaper: {
+    backgroundColor: "transparent" as const, 
+    verticalAlign: "middle" as const,
+    textAlign: "center" as const,
+    margin: 1 as const,
+    width: "15vw" as const,
+    height: "40vh" as const,
+    border: "solid 4px grey" as const,
+    borderRadius: "20px" as const,
+    boxShadow: "1px 3px 1px grey",
+  },
   column: {
     paddingTop: "5vh" as const,
   },
@@ -51,18 +67,27 @@ const styles = {
     marginTop: "2vh" as const,
     marginBottom: "1vh" as const,
     fontWeight: "bold" as const,
-    border: "solid 3px #831869",
+    border: "solid 2px #831869",
     fontColor: "#831869" as const,
     color: "#514384" as const,
     boxShadow: "1px 2px 1px grey",
   },
   carCatalog: {
     marginLeft: "5vw",
+  },
+  filterBox: {
+    marginLeft: "5vw",
   }
 };
 
+function valuetext(value: number) {
+  return `${value}`;
+}
+
 export default class App extends React.Component {
   state = {
+    inputDuration: 1,
+    inputDistance: 50,
     cars: [],
   };
 
@@ -72,13 +97,27 @@ export default class App extends React.Component {
     });
   }
 
+  updateQuery = () => {
+      axios.get("/cars.json", {
+        params: {
+          distance: this.state.inputDistance,
+          duration: this.state.inputDuration,
+        }
+      }).then((response) => {
+        this.setState({ cars: response.data });
+      });
+  }
+
+  handleDurationChange = (event, value) => this.setState({ inputDuration: value });
+  handleDistanceChange = (event, value) => this.setState({ inputDistance: value });
+
   CarCatalog = () => {
     const { cars } = this.state;
     return (
       <>
           {cars.map((car) => (
             <Col span={3} style= {styles.column}>
-              <Paper style={styles.paper}>
+              <Paper style={styles.carPaper}>
                       <img src={`${car.picturePath}`} width="80%" style= {styles.picture} /> <br />
                       <h1>{car.brand} {car.model}</h1>
                       <CarInfo><b>${car.pricePerDay}</b>/day</CarInfo>
@@ -89,6 +128,45 @@ export default class App extends React.Component {
               </Paper>
             </Col>
           ))}
+      </>
+    );
+  };
+
+  CarFilters = () => {
+    return (
+      <>
+            <Col span={3} style= {styles.column}>
+              <Paper style={styles.filtersPaper}>
+                      <h1>Filters</h1>
+                      <FilterInfo><b>Duration (days):</b></FilterInfo>
+                      <Slider
+                      defaultValue={5}
+                      getAriaValueText={valuetext}
+                      aria-labelledby="discrete-slider-restrict"
+                      step={1}
+                      valueLabelDisplay="auto"
+                      min={1}
+                      max={30}
+                      marks
+                      onChange={this.handleDurationChange}
+                    />
+                      <FilterInfo><b>Distance (km):</b></FilterInfo>
+                      <Slider
+                      defaultValue={5}
+                      getAriaValueText={valuetext}
+                      aria-labelledby="discrete-slider-restrict"
+                      step={50}
+                      valueLabelDisplay="auto"
+                      min={50}
+                      max={3000}
+                      marks
+                      onChange={this.handleDistanceChange}
+                    />
+                      <Button variant="outlined" color="primary" style= {styles.bookButton} size= "large" onClick={this.updateQuery}>
+                       SEARCH
+                        </Button>
+              </Paper>
+            </Col>
       </>
     );
   };
@@ -105,6 +183,11 @@ export default class App extends React.Component {
             <Logo src={Deezer} />
           </HeaderContainer>
         </div>
+        <Col span={3}>
+          <div style= {styles.filterBox}>
+            <this.CarFilters />
+          </div>
+        </Col>
         <div style={styles.carCatalog}>
           <Row gutter={30}>
             <this.CarCatalog />
